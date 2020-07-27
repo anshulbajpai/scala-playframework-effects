d9951d5b2c71aad5376778a41adf474bac8aa48a
Anshul Bajpai <bajpai.anshul@gmail.com>
2020-07-27 20:43:00 +0100

    build refactoring

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,18 +3,21 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val playVersion = "2.8.2"
-    val catsEffectVersion = "2.1.4"
-    val simulacrumVersion = "1.0.0"
+    val playVersion          = "2.8.2"
+    val catsEffectVersion    = "2.1.4"
+    val simulacrumVersion    = "1.0.0"
     val scalaTestPlayVersion = "5.1.0"
+    val macwireVersion       = "2.3.6"
+    val macroParadiseVersion = "2.1.1"
   }
 
   import Versions._
 
-  val play = "com.typesafe.play" %% "play" % playVersion
-  val catsEffect = "org.typelevel" %% "cats-effect" % catsEffectVersion
-  val simulacrum = "org.typelevel" %% "simulacrum" % simulacrumVersion
-  val macwire = "com.softwaremill.macwire" %% "macros" % "2.3.6" % "provided"
-  val scalaTestPlay = "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlayVersion % "test"
+  val play          = "com.typesafe.play"        %% "play"               % playVersion
+  val catsEffect    = "org.typelevel"            %% "cats-effect"        % catsEffectVersion
+  val simulacrum    = "org.typelevel"            %% "simulacrum"         % simulacrumVersion
+  val macwire       = "com.softwaremill.macwire" %% "macros"             % macwireVersion % "provided"
+  val scalaTestPlay = "org.scalatestplus.play"   %% "scalatestplus-play" % scalaTestPlayVersion % "test"
+  val macroParadise = "org.scalamacros"          % "paradise"            % macroParadiseVersion cross CrossVersion.full
 
 }
